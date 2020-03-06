2e0c9b510ad2ba461c54e085948b6722d9176b4f
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-03-06 10:20:05 +0100

    init

---
+++ src/Covid.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import $ from "jquery";
+import { Doughnut } from 'react-chartjs-2';
+
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import './assets/stylesheets/style.scss';
+
+const BASE_API = "https://covid2019-api.herokuapp.com/";
+
+class Covid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      world: {
+        confirmed: 0,
+        deaths: 0,
+        recovered: 0
+      },
+      china: {
+        confirmed: 0,
+        deaths: 0,
+        recovered: 0
+      },
+      rest: {
+        confirmed: 0,
+        deaths: 0,
+        recovered: 0    
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.setState({isLoading: true}, () => {
+      $.ajax({
+        url: `${BASE_API}/total`,
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        dataType: "text json",
+        crossDomain: false,
+        success: function(response) {
+          this.setState({
+              isLoading: false,
+              world: {
+                confirmed: response.confirmed,
+                deaths: response.deaths,
+                recovered: response.recovered
+              }
+          });
+        }.bind(this),
+        error: function(_xhr, _status, _err) {
+          this.setState({ isLoading: false });
+        }.bind(this)
+      });   
+    });
+  }
+
+
+  render() {
+    let {confirmed, deaths, recovered} = this.state.world;
+    let active = confirmed - deaths - recovered;
+
+    let data = {
+      datasets: [{
+          data: [active, deaths, recovered]
+      }],
+      labels: [
+        'Active',
+        'Deaths',
+        'Recovered'
+      ]
+    };
+
+    return (
+      
+      <div className="columns">
+        <div className="column" id="world">
+          <h2>World</h2>
+          <div className="columns">
+            <div className="column">
+              <Doughnut data={data} />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const yieldNode = document.querySelector("#yield");
+
+ReactDOM.render(
+  <Covid />,
+  yieldNode
+);
---
+++ webpack.prod.js
@@ -0,0 +1,89 @@
+const { resolve } = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const cssLoaders =
+  [
+    {
+      loader: 'css-loader',
+      options: { importLoaders: 1 }
+    }
+  ];
+
+let config = {
+  entry: resolve('./src/Covid.js'),
+  mode: 'production',
+  watch: true,
+  output: {
+    path: resolve('./dist'),
+    filename: 'bundle.min.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [
+      resolve('./node_modules'),
+      resolve('./src')
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: cssLoaders
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [...cssLoaders, "sass-loader"]
+        })
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf|wav)(\?.*)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)(\?.*)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: { 
+              limit: 8192, 
+              outputPath: "./src/assets/images/"
+            }
+          },
+          {
+            loader: "img-loader",
+            options: { 
+              enabled: false,
+              outputPath: "./src/assets/images/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'stylesheets/application.min.css',
+      disable: true
+    }),
+  ],
+  devServer: {
+    contentBase: resolve("./"),
+    compress: true,
+    historyApiFallback: true,
+    port: 3210
+  }
+};
+
+module.exports = config
