57a324c5f9f7ed2ff19fc788cf8229be3f20a19c
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-03-06 13:59:08 +0100

    add charts

--- src/Covid.js
+++ src/Covid.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import $ from "jquery";
-import { Doughnut } from 'react-chartjs-2';
-
+import { Doughnut, Pie, Bar } from 'react-chartjs-2';
+import "chartjs-plugin-datalabels";
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './assets/stylesheets/style.scss';
 
@@ -19,14 +19,9 @@ class Covid extends Component {
         recovered: 0
       },
       china: {
-        confirmed: 0,
-        deaths: 0,
-        recovered: 0
-      },
-      rest: {
-        confirmed: 0,
-        deaths: 0,
-        recovered: 0    
+        confirmedChina: 0,
+        deathsChina: 0,
+        recoveredChina: 0
       }
     }
   }
@@ -36,59 +31,238 @@ class Covid extends Component {
       $.ajax({
         url: `${BASE_API}/total`,
         type: "GET",
-        contentType: "application/json; charset=utf-8",
-        dataType: "text json",
-        crossDomain: false,
+        dataType: "json",
+        crossDomain: true,
         success: function(response) {
+          let res = response;
+
+          let world = {
+            confirmed: res.confirmed,
+            deaths: res.deaths,
+            recovered: res.recovered  
+          }
+
           this.setState({
-              isLoading: false,
-              world: {
-                confirmed: response.confirmed,
-                deaths: response.deaths,
-                recovered: response.recovered
-              }
+            isLoading: false,
+            world: world
           });
         }.bind(this),
         error: function(_xhr, _status, _err) {
           this.setState({ isLoading: false });
         }.bind(this)
       });   
+
+      $.ajax({
+        url: `${BASE_API}/country/Mainland_China`,
+        type: "GET",
+        dataType: "json",
+        crossDomain: true,
+        success: function(response) {
+          let resChina = response.Mainland_China;
+
+          let china = {
+            confirmedChina: resChina.confirmed,
+            deathsChina: resChina.deaths,
+            recoveredChina: resChina.recovered  
+          }
+
+          this.setState({
+            isLoading: false,
+            china: china
+          });
+        }.bind(this),
+        error: function(_xhr, _status, _err) {
+          this.setState({ isLoading: false });
+        }.bind(this)
+      }); 
     });
   }
 
 
   render() {
+    console.log(this.state);
+
     let {confirmed, deaths, recovered} = this.state.world;
     let active = confirmed - deaths - recovered;
 
-    let data = {
+
+
+    let {confirmedChina, deathsChina, recoveredChina} = this.state.china;
+    let activeChina = confirmedChina - deathsChina - recoveredChina;
+
+    console.log(this.state.china.confirmed)
+
+    let worldGeneralData = {
       datasets: [{
-          data: [active, deaths, recovered]
+        backgroundColor: [
+          "#f38b4a",
+          "#ff8397",
+          "#56d798"
+        ],
+        data: [active, deaths, recovered]
       }],
       labels: [
         'Active',
         'Deaths',
         'Recovered'
-      ],        
-      backgroundColor: [
-        "#f38b4a",
-        "#56d798",
-        "#ff8397"
-      ],
+      ]
     };
 
+    let worldGeneralWithoutActiveData = {
+      datasets: [{
+        backgroundColor: [
+          "#ff8397",
+          "#56d798"
+        ],
+        data: [deaths, recovered]
+      }],
+      labels: [
+        'Deaths',
+        'Recovered'
+      ]
+    }
+
+    let chinaGeneralData = {
+      datasets: [{
+        backgroundColor: [
+          "#f38b4a",
+          "#ff8397",
+          "#56d798"
+        ],
+        data: [activeChina, deathsChina, recoveredChina]
+      }],
+      labels: [
+        'Active',
+        'Deaths',
+        'Recovered'
+      ]
+    };
+    
+    let chinaGeneralWithoutActiveData = {
+      datasets: [{
+        backgroundColor: [
+          "#ff8397",
+          "#56d798"
+        ],
+        data: [deathsChina, recoveredChina]
+      }],
+      labels: [
+        'Deaths',
+        'Recovered'
+      ]
+    }
+
+    let excludingChinaGeneralData = {
+      datasets: [{
+        backgroundColor: [
+          "#f38b4a",
+          "#ff8397",
+          "#56d798"
+        ],
+        data: [active - activeChina, deaths - deathsChina, recovered - recoveredChina]
+      }],
+      labels: [
+        'Active',
+        'Deaths',
+        'Recovered'
+      ]
+    };
+
+    let excludingChinaGeneralWithoutActiveData = {
+      datasets: [{
+        backgroundColor: [
+          "#ff8397",
+          "#56d798"
+        ],
+        data: [deaths - deathsChina, recovered - recoveredChina]
+      }],
+      labels: [
+        'Deaths',
+        'Recovered'
+      ]
+    }
+
+    let options = {
+      tooltips: {
+        enabled: false
+      },
+      plugins: {
+        datalabels: {
+          color: "#FFF"
+        }
+      }
+    }
+
+    let percentOptions = {
+      tooltips: {
+        enabled: false
+      },
+      plugins: {
+        datalabels: {
+          formatter: (value, ctx) => {
+            let datasets = ctx.chart.data.datasets;
+   
+            if (datasets.indexOf(ctx.dataset) === datasets.length - 1) {
+              let sum = datasets[0].data.reduce((a, b) => a + b, 0);
+              let percentage = Math.round((value / sum) * 100) + '%';
+              return percentage;
+            } else {
+              return percentage;
+            }
+          },
+          color: '#fff',
+        }
+      }
+    }
+
     return (
-      
-      <div className="columns">
-        <div className="column" id="world">
-          <h2>World</h2>
-          <div className="columns">
-            <div className="column">
-              <Doughnut data={data} />
+      <div>
+        <div className="columns">
+          <div className="column panel" id="world">
+            <h2>World</h2>
+            <div className="columns">
+              <div className="column sub-panel">
+                <h3>All</h3>
+                <Pie data={worldGeneralData} options={options}/>
+              </div>
+              <div className="column sub-panel">
+                <h3>Without active</h3>
+                <Pie data={worldGeneralWithoutActiveData} options={percentOptions} />
+              </div>
             </div>
           </div>
         </div>
+        <div className="columns">
+          <div className="column panel" id="china">
+            <h2>China</h2>
+            <div className="columns">
+              <div className="column sub-panel">
+                <h3>All</h3>
+                <Pie data={chinaGeneralData} options={options}/>
+              </div>
+              <div className="column sub-panel">
+                <h3>Without active</h3>
+                <Pie data={chinaGeneralWithoutActiveData} options={percentOptions} />
+              </div>
+            </div>
+          </div>
+          <div className="column panel" id="china">
+            <h2>Excluding China</h2>
+            <div className="columns">
+              <div className="column sub-panel">
+                <h3>All</h3>
+                <Pie data={excludingChinaGeneralData} options={options}/>
+              </div>
+              <div className="column sub-panel">
+                <h3>Without active</h3>
+                <Pie data={excludingChinaGeneralWithoutActiveData} options={percentOptions} />
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
+
     )
   }
 }
